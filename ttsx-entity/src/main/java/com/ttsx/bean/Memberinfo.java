c1a0a5a9d6c0eb7c9279195567f441d45d850683
ljj218799 <goole.ljj@gmail.com>
2023-05-07 20:39:43 +0800

    foods_order修改

--- ttsx-entity/src/main/java/com/ttsx/bean/Memberinfo.java
+++ ttsx-entity/src/main/java/com/ttsx/bean/Memberinfo.java
@@ -1,4 +1,4 @@
-package bean;
+package com.ttsx.bean;
 
 import lombok.Data;
 
--- ttsx-entity/src/main/java/com/ttsx/bean/Orderinfo.java
+++ ttsx-entity/src/main/java/com/ttsx/bean/Orderinfo.java
@@ -1,9 +1,8 @@
-package bean;
+package com.ttsx.bean;
 
 import lombok.Data;
 
 import java.io.Serializable;
-import java.util.List;
 
 /**
  * @Author: mqb
