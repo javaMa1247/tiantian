0aecf2bc6d1d976f368597aef5b0ca7df50e63f6
ljj218799 <goole.ljj@gmail.com>
2023-05-11 14:17:19 +0800

    index

--- ttsx-index/src/main/java/com/ttsx/index/IndexApplication.java
+++ ttsx-index/src/main/java/com/ttsx/index/IndexApplication.java
@@ -3,6 +3,7 @@ package com.ttsx.index;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @Description:
